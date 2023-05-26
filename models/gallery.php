d7a31ed41870a6a8dd6b042312b4a24a81cdb114
Fábio cabral <fabio.brito.cabral@gmail.com>
2023-05-26 13:26:57 +0100

    Conexao da base de dados com a view gallery

---
+++ models/gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('models/base.php');
+
+class Gallery extends Base
+{
+
+    public function get()
+    {
+        $query = $this->db->prepare("
+            SELECT gallery_id, photo
+            FROM gallery
+        ");
+
+        $query->execute();
+
+        return $query->fetchAll();
+
+    }
+}
